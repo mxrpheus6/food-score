326996bf268f4001632306affab7a048b697b1d1
mxrpheus6 <pkazachenkoo@gmail.com>
2025-05-27 23:10:03 +0300

    Change HTTP method from PUT to PATCH to follow the implementation logic

--- product-service/src/main/java/com/mxrpheus/productservice/controller/impl/BrandController.java
+++ product-service/src/main/java/com/mxrpheus/productservice/controller/impl/BrandController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -53,7 +54,7 @@ public class BrandController implements BrandOperations {
     }
 
     @Override
-    @PutMapping("/{brandId}")
+    @PatchMapping("/{brandId}")
     @Validated(Marker.OnUpdate.class)
     public BrandResponse updateBrandById(@PathVariable Long brandId,
                                          @RequestBody @Valid BrandRequest brandRequest) {
